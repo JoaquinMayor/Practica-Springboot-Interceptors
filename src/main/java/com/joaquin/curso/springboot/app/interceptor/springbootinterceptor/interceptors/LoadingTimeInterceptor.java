1024650446eec7759f1aacc2e0d01b69d2f8c53c
JoaquinMayor <joaquin88mayor@gmail.com>
2024-01-10 20:59:14 -0300

    w

--- src/main/java/com/joaquin/curso/springboot/app/interceptor/springbootinterceptor/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/joaquin/curso/springboot/app/interceptor/springbootinterceptor/interceptors/LoadingTimeInterceptor.java
@@ -23,34 +23,34 @@ public class LoadingTimeInterceptor implements HandlerInterceptor{ //Siempre imp
     private static final Logger logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
 
 
-    @Override //El request podemos recibir datos de la peticion y el resonse es para que nosostros mandemos mensajes, el handler es el controlador
+    @Override //El request podemos recibir datos de la petición y el resonse es para que nosostros mandemos mensajes, el handler es el controlador
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-            throws Exception { //Se carga antes de cargar la pagina
+            throws Exception { //Se carga antes de cargar la página
                 HandlerMethod controler = ((HandlerMethod) handler);
         logger.info("LoadingTimeInterceptor: preHandle() entrando..."+ controler.getMethod().getName());
 
         long start = System.currentTimeMillis(); //Nos guarda momento de inicio en milisegundos
-        request.setAttribute("start", start);//guardamos el start en el request porque es lo que se comparte entre el pre y el post y asi ver cuanto se tarda
+        request.setAttribute("start", start);//Guardamos el start en el request porque es lo que se comparte entre el pre y el post y así ver cuanto se tarda
 
         Random random = new Random();
         int delay = random.nextInt(500); //Rango de valor entre 0 y 500
         Thread.sleep(delay); 
 
-        Map<String, String> json = new HashMap<>(); //Estas lineas de codigo es por si es falso, que retorne un json de error
+        Map<String, String> json = new HashMap<>(); //Estas lineas de código es por si es falso, que retorne un json de error
         json.put("error", "No tiene acceso a esta pagina!");
         json.put("date", new Date().toString());
         ObjectMapper mapper = new ObjectMapper();
-        String jsonString = mapper.writeValueAsString(json); //lo transforma en un json
-        response.setContentType("application/json"); //le decimos que le mandamos un json
-        response.setStatus(401); //determinamos el status del error
-        response.getWriter().write(jsonString); //agregamos el json para mandar
+        String jsonString = mapper.writeValueAsString(json); //Lo transforma en un json
+        response.setContentType("application/json"); //Le decimos que le mandamos un json
+        response.setStatus(401); //Determinamos el status del error
+        response.getWriter().write(jsonString); //Agregamos el json para mandar
 
-      return false; //si retorna false no se ejecuta la pagina y restringe el acceso al controlador
+      return false; //Si retorna false no se ejecuta la pagina y restringe el acceso al controlador
     } 
     
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-            ModelAndView modelAndView) throws Exception { //Se ejecuta al terminar la carga de la pagina
+            ModelAndView modelAndView) throws Exception { //Se ejecuta al terminar la carga de la página
         long end = System.currentTimeMillis();
         long start = (long)request.getAttribute("start");
         long result = end - start;
